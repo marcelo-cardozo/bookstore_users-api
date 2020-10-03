f75239f2862c16ccacb04b4a9d105ee97582607c
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-10-02 22:48:03 -0400

    Initail commit

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,14 @@
+package users
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func CreateUser(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, "")
+}
+
+func GetUser(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, "")
+}
