87fcc90d881ed80545b932112af9c05f41766643
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-10-04 16:28:54 -0300

    Add create and get user endpoints

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,13 +2,56 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/marcelo-cardozo/bookstore_users-api/domain/users"
+	"github.com/marcelo-cardozo/bookstore_users-api/service"
+	"github.com/marcelo-cardozo/bookstore_users-api/utils/errors"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "")
+	var user users.User
+
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	c.JSON(http.StatusBadRequest, err)
+	//	return
+	//}
+	//
+	//if err := json.Unmarshal(bytes, &user); err != nil {
+	//	c.JSON(http.StatusBadRequest, err)
+	//	return
+	//}
+
+	// or
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestRestErr(err.Error())
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	createdUser, restErr := service.UsersService.CreateUser(user)
+	if restErr != nil {
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, createdUser)
 }
 
 func GetUser(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "")
+	userId, err := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if err != nil {
+		restErr := errors.NewBadRequestRestErr("used id should be a number")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user, restErr := service.UsersService.GetUser(userId)
+	if restErr != nil {
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
--- service/users_service.go
+++ service/users_service.go
@@ -1,1 +1,47 @@
 package service
+
+import (
+	"github.com/marcelo-cardozo/bookstore_users-api/domain/users"
+	"github.com/marcelo-cardozo/bookstore_users-api/utils/errors"
+)
+
+type usersService struct {
+}
+
+type usersServiceInterface interface {
+	CreateUser(users.User) (*users.User, *errors.RestErr)
+	GetUser(int64) (*users.User, *errors.RestErr)
+}
+
+var (
+	UsersService usersServiceInterface
+)
+
+func init() {
+	UsersService = &usersService{}
+}
+
+func (s *usersService) CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *usersService) GetUser(userId int64) (*users.User, *errors.RestErr) {
+	if userId < 0 {
+		return nil, errors.NewBadRequestRestErr("user id should be greater than 0")
+	}
+
+	user := users.User{Id: userId}
+	if err := user.Get(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
